c727a879798c36f5c88347e8e674e173fdb2cd70
Erik Garrison <erik.garrison@gmail.com>
2022-12-12 17:32:05 +0000

    back to cosine distance

--- cosigt.py
+++ cosigt.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 import itertools
 from scipy import spatial
-from scipy.special import rel_entr
 from operator import itemgetter
 
 
@@ -22,20 +21,16 @@ def main(bin_tsv,sam_tsv,outf):
     combos=list(itertools.combinations(enumerate(haplos),2))
     #initialize empty array
     #with no normalization, ok to use int
-    #matrix = np.zeros(shape=(len(combos),binary_df.shape[1]),dtype=int)
+    matrix = np.zeros(shape=(len(combos),binary_df.shape[1]),dtype=int)
     # recode zeros in input vectors to this value
-    # this is required for the KL-divergence calculation
-    zero_weight=0.1
+    #zero_weight=-1e3
     #with normalization, we have to use floats
-    matrix = np.zeros(shape=(len(combos),binary_df.shape[1]),dtype=np.float32)
+    #matrix = np.zeros(shape=(len(combos),binary_df.shape[1]),dtype=np.float32)
 
     #iterate over the combinations and get the sum of corresponding values
     for i,combo in enumerate(combos):
         matrix[i]=binary_ar[combo[0][0]] + binary_ar[combo[1][0]]
-        matrix[i]=np.where(matrix[i]==0, zero_weight, matrix[i])
-        # and normalize for KL-divergence
-        matrix[i]=matrix[i]/matrix[i].sum()
-
+        #matrix[i]=np.where(matrix[i]==0, zero_weight, matrix[i])
     #store as table
     combo_df = pd.DataFrame(matrix,columns=binary_df.columns.tolist())
     combo_df.index = [combo[0][1] + "/" +combo[1][1] for combo in combos]
@@ -51,19 +46,14 @@ def main(bin_tsv,sam_tsv,outf):
     for idx,row in cov_tsv.iterrows():
         
         s_results=list()
-        row=np.where(row==0, zero_weight, row)
-        row=row/row.sum()
-        #row=row.tolist()
+        #row=np.where(row==0, zero_weight, row)
 
         for idx2,row2 in combo_df.iterrows():
-            #cos_sim=1-spatial.distance.cosine(row, row2)
-            #s_results.append((idx,idx2,cos_sim))
-            kl_div=sum(rel_entr(row, row2))
-            s_results.append((idx,idx2,kl_div))
-            #print("kl_div is", kl_div)
+            cos_sim=1-spatial.distance.cosine(row, row2)
+            s_results.append((idx,idx2,cos_sim))
 
         matrix_cov[count] = [x[2] for x in s_results]
-        results.append(sorted(s_results,key=itemgetter(2),reverse=False)[0])
+        results.append(sorted(s_results,key=itemgetter(2),reverse=True)[0])
         count+=1
         
     #convert to df
