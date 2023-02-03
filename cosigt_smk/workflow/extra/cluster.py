96b44aa7a33c7026cf37f5d55e93175b57f6d3a4
davidebolo1993 <davidebolognini7@gmail.com>
2023-02-03 14:27:58 +0100

    cutting tree at different tresholds and more files for evaluation

--- cosigt_smk/workflow/extra/cluster.py
+++ cosigt_smk/workflow/extra/cluster.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
-import editdistance
-from Bio import Phylo
-from sklearn.metrics.pairwise import pairwise_distances
-from sklearn.cluster import DBSCAN
 import numpy as np
 import json 
 import sys
 import os
+import editdistance
+from Bio import Phylo
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.cluster import AgglomerativeClustering,DBSCAN
+from scipy.cluster import hierarchy
+from scipy.cluster.hierarchy import dendrogram,cut_tree
+import matplotlib.pyplot as plt
 
+
+"""
 def editdist(x,y):
 
 	'''
@@ -16,7 +21,6 @@ def editdist(x,y):
 	'''
 
 	return int(editdistance.eval(data[int(x[0])], data[int(y[0])]))
-
 
 
 def diffperc(x,y):
@@ -27,6 +31,7 @@ def diffperc(x,y):
 
 	return 100*int(editdistance.eval(data[int(x[0])], data[int(y[0])]))/max(len(data[int(x[0])]),len(data[int(y[0])]))
 
+"""
 
 def read_newick(nwk):
 
@@ -64,11 +69,11 @@ def read_tsv(tsv):
 
 	return d
 
-
-def cluster_tree(tree,affinity):
+"""
+def dbscan_tree(tree,affinity):
 
 	'''
-	Cluster clades
+	Cluster clades using DBSCAN
 	'''
 	#output
 	result=[]
@@ -96,23 +101,39 @@ def cluster_tree(tree,affinity):
 	return result
 
 
-""" def label_dendrogram(tsv_dict,tree):
+
+def agglomerative_tree(tree,n):
 
 	'''
-	Assign labels to original dendrogram
+	Cluster clades using agglomerative clustering
 	'''
 
-	d={}
+	#output
+	result=[]
+	
+	#input, make it global
+	global data
+	data=[]
 
 	for clade in tree.get_terminals():
 	
 		key = clade.name
-		d[key]=tsv_dict[key]
+		data.append(key)
 
-	return d
- """
+	X = np.arange(len(data)).reshape(-1, 1)
+	metric= pairwise_distances(X, X, metric=editdist)
+	agg = AgglomerativeClustering(distance_threshold=None, n_clusters=n,affinity='precomputed',linkage='average')
+	cluster_=agg.fit(metric)
+	groups=set(cluster_.labels_)
 
+	for g in groups:
+		
+		group=list(np.take(data,np.where(cluster_.labels_ == g))[0])
+		result.append(group)
 
+	return result
+
+"""
 
 def label_clustering_results(tsv_dict,cluster_results):
 
@@ -168,7 +189,123 @@ def proximities(cluster):
 
 	return d
 
+
+def TreeToLinkage(tree):
 	
+	'''
+	Convert tree to Linkage matrix
+	'''
+
+	tree_height = max(tree.distance(c) for c in tree.find_clades(terminal=True))
+
+	# add comment with id and span of terminal nodes:
+	id_map = {}
+
+	for i, c in enumerate(tree.find_clades(terminal=True)):
+
+		c.comment = (i, 1) 
+		id_map[i] = c.name
+
+	# ancestor list orderred by distance from the root
+
+	anc_lst = []
+
+	for c in tree.find_clades(terminal=False):
+
+		d = tree.distance(c)
+		anc_lst.append((c, list(c), d))
+
+	anc_lst.sort(key=lambda x:x[2], reverse=True)
+
+	# running number of node
+
+	nodes = len(list(tree.find_clades(terminal=True)))
+	lnk_lst = []
+	for anc,children, anc_d in anc_lst:
+
+		n_children = len(children)
+		assert n_children>=2
+		child1 = children[0]
+
+		for child2 in children[1:]:
+
+			id1, n_leaves1 = child1.comment
+			id2, n_leaves2 = child2.comment
+			total_leaves = n_leaves1 + n_leaves2
+			anc.comment = (nodes, total_leaves)
+			distance = tree_height - anc_d
+			nodes += 1
+			row = [id1, id2, distance, total_leaves]
+			lnk_lst.append(row)
+			child1 = anc
+
+	return np.array(lnk_lst), id_map
+
+
+def GroupBundles(ids_old, idx_new):
+
+	'''
+	Re-group bundles
+	'''
+
+	m=dict()
+
+	for i,idx in enumerate(idx_new):
+
+		if idx in m:
+
+			m[idx].append(ids_old[i])
+
+		else:
+
+			m[idx] = [ids_old[i]]
+
+	return m
+
+
+def GroupIds(idx_new):
+
+	'''
+	Re-group ids
+	'''
+
+	m=dict()
+
+	for i,idx in enumerate(idx_new):
+
+		if idx in m:
+
+			m[idx].append(str(i))
+
+		else:
+
+			m[idx] = [str(i)]
+
+	return m
+
+
+
+def Recluster(groups):
+
+	'''
+	Re-cluster ids
+	'''
+
+	result=[]
+
+	for key in groups:
+
+		group=[]
+
+		for val in groups[key]:
+
+			group.append(val)
+
+		result.append(group)
+
+	return result
+
+
 
 def main():
 
@@ -176,28 +313,86 @@ def main():
 	Run
 	'''
 
+	#deal with input files
 	nwk=os.path.abspath(sys.argv[1])
 	tsv=os.path.abspath(sys.argv[2])
 	outpath=os.path.abspath(sys.argv[3])
 
+	#read newick
 	tree=read_newick(nwk)
 	dendro_labels=read_tsv(tsv)
-	outcluster(dendro_labels,outpath + "/dendrogram.clusters.tsv")
+
+	#no cluster
+	outcluster(dendro_labels,outpath + "/dendrogram.nocut.tsv")
 	prox_dendr=proximities(dendro_labels)
 
-	with open(outpath + "/dendro.proxi.json", "w") as outfile:
+	with open(outpath + "/dendrogram.nocut.json", "w") as outfile:
 		
 		json.dump(prox_dendr, outfile,indent = 4)
 
-	new_clust=cluster_tree(tree,90.0)
+	#re-build the linkage matrix to use with scipy and cut the dendrogram
+	linkage,ids=TreeToLinkage(tree)
+	plt.figure()
+	dendrogram = hierarchy.dendrogram(linkage)
+	plt.savefig(outpath + "/dendrogram.nocut.png")
+	plt.close()
+
+	#cut at predefined treshold - manual cut
+	for d in reversed(range(2,15,1)):
+
+		cut_linkage=cut_tree(linkage, n_clusters=d).reshape(-1,)
+		cut_groups=GroupBundles(ids,cut_linkage)
+		cut_ids=GroupIds(cut_linkage)
+
+		plt.figure()
+		dendrogram = hierarchy.dendrogram(linkage, truncate_mode = 'lastp', p = d)
+		plt.savefig(outpath + "/dendrogram.cut"+ str(d) + ".png")
+		plt.close()
+
+		with open(outpath + "/dendrogram.cut"+ str(d) + ".ids.tsv", "w") as outfile:
+
+			for ix in cut_ids:
+				
+				outfile.write(",".join(cut_ids[ix]) + "\n")
+
+		cut_cluster=Recluster(cut_groups)
+		cut_cluster_labels=label_clustering_results(dendro_labels,cut_cluster)
+		outcluster(cut_cluster_labels, outpath + "/dendrogram.cut"+ str(d) + ".tsv")
+		prox_clust=proximities(cut_cluster_labels)
+
+		with open(outpath + "/dendrogram.cut"+ str(d) + ".json", "w") as outfile:
+		
+			json.dump(prox_clust, outfile, indent = 4)
+
+	"""
+	#not used right now
+
+	#clustering based on string similarity
+	#recluster based on string similarity - AGGLOMERATIVE clustering - ngroups
+	n=14
+	new_clust=agglomerative_tree(tree,n)
 	cluster_labels=label_clustering_results(dendro_labels,new_clust)
-	outcluster(cluster_labels, outpath + "/DBSCAN.clusters.tsv")
+	outcluster(cluster_labels, outpath + "/agglomerative_" + str(n) +".tsv")
 	prox_clust=proximities(cluster_labels)
 
-	with open(outpath + "/DBSCAN.proxi.json", "w") as outfile:
+	with open(outpath +"/agglomerative_" + str(n) +".json", "w") as outfile:
 		
 		json.dump(prox_clust, outfile, indent = 4)
 
+
+	#recluster based on string similarity - DBSCAN clustering - similarity teshold
+	n=88
+	new_clust=dbscan_tree(tree,n)
+	cluster_labels=label_clustering_results(dendro_labels,new_clust)
+	outcluster(cluster_labels, outpath + "/dbscan_" + str(n) +".tsv")
+	prox_clust=proximities(cluster_labels)
+
+	with open(outpath +"/dbscan_" + str(n) +".json", "w") as outfile:
+		
+		json.dump(prox_clust, outfile, indent = 4)
+
+	"""
+	
 
 
 if __name__ == '__main__':
