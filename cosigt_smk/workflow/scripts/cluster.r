f8581b4208297dabcaa4606e73368254da9da0da
davidebolo1993 <davidebolognini7@gmail.com>
2024-10-14 10:59:26 +0200

    fix type in cluster.r script [ci skip]

--- cosigt_smk/workflow/scripts/cluster.r
+++ cosigt_smk/workflow/scripts/cluster.r
@@ -20,7 +20,7 @@ distanceMatrix <- as.dist(regularMatrix)
 #calculate silhouette score and best partition
 res<-NbClust(diss=distanceMatrix, method = "average", index ="silhouette", distance=NULL)$Best.partition
 res.list <- lapply(split(res, names(res)), unname)
-named_res<-lapply(res.list, function(x, prefix) paste0(prefix, x), prefix = "HploGroup")
+named_res<-lapply(res.list, function(x, prefix) paste0(prefix, x), prefix = "HaploGroup")
 jout<-toJSON(named_res)
 #write json out
 write(jout, args[2])
