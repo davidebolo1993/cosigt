860d5b5784d3a008cec767c61836c32f698db662
davidebolo1993 <davidebolognini7@gmail.com>
2024-10-28 14:32:53 +0100

    mod plotcoverage.r to adapt to different number of inputs [ci skip]

--- cosigt_smk/workflow/scripts/plotcoverage.r
+++ cosigt_smk/workflow/scripts/plotcoverage.r
@@ -7,9 +7,11 @@ args <- commandArgs(trailingOnly = TRUE)
 
 bw <- import.bw(args[1])
 df<- data.frame(bw)
+df$seqnames<-do.call(c,lapply(as.character(df$seqnames), function(x) unlist(strsplit(x,":", fixed=T))[1]))
+n<-length(unique(df$seqnames))
 p<-ggplot(df, aes(x=start, y=score, group=seqnames)) +
     geom_point(show.legend=F) + 
     geom_smooth(show.legend=F) + 
-    facet_wrap(~seqnames, ncol=5, scales="free_x") +
+    facet_wrap(~seqnames, ncol=10, scales="free_x") + #up to 10 columns - no more 
     theme_linedraw()
-ggsave(args[2],width=20, height=20)
+ggsave(args[2],width=30, height=3*round(n/10))
