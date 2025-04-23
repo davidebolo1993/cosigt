3d75d335dc3c5c6b6a7b5d8b253b53e75ad4749c
Thomas Sumner Dudley (ext) <thomas.dudley@hnode02.cm.cluster>
2025-04-23 10:22:23 +0200

    fix config key in checkpoint wfmash.smk and remove unused organize.py output file from the original repo

--- cosigt_smk/cosigt_smk.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-SINGULARITY_TMPDIR=/scratch/davide.bolognini snakemake --profile config/slurm --singularity-args "-B /scratch/davide.bolognini,/group/soranzo/davide.bolognini/working/dev/cosigt_paper/real_data -e" cosigt
