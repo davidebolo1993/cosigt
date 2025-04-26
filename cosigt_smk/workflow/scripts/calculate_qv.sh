03384683bd1cae5657b968ffff0e5082f46c68dc
Andrea Guarracino <62253982+AndreaGuarracino@users.noreply.github.com>
2025-04-26 12:03:03 -0500

    Fix paths in `calculate_qv.sh`

--- cosigt_smk/workflow/scripts/calculate_qv.sh
+++ cosigt_smk/workflow/scripts/calculate_qv.sh
@@ -11,7 +11,7 @@ fi
 cat "$indir/ids.tsv" | while read line; do
     fa1=$(echo "$line" | cut -d$'\t' -f 1)
     fa2=$(echo "$line" | cut -d$'\t' -f 2)
-    QV=$(compute_qv "$fa1" "$fa2")
+    QV=$(compute_qv "$indir/$fa1" "$indir/$fa2")
     echo -e "$fa1\t$fa2\t$QV" >> "$indir/qv.tmp.tsv"
 done
 
