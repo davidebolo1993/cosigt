b5eef700ec634bab4eeb47c981049ca1a0fe5d48
davidebolo1993 <davidebolognini7@gmail.com>
2025-06-09 12:19:51 +0200

    change bedpe merging logic

--- cosigt_smk/workflow/scripts/bedpe_merge.sh
+++ cosigt_smk/workflow/scripts/bedpe_merge.sh
@@ -6,26 +6,29 @@ distance="$2"
 awk -v d="$distance" '
 function flush() {
   if (qname != "") {
-    print qname, qstart, qend, tname, tstart, tend, ".", "0", "+", "+" 
+    print qname, qstart, qend, tname, tstart, tend, ".", "0", qstrand, tstrand
   }
 }
 BEGIN { OFS = "\t" }
 {
   if ($1 == qname && $4 == tname &&
-      $2 - qend <= d && $5 - tend <= d) {
-    # Extend current block
-    qend = ($3 > qend ? $3 : qend)
-    tend = ($6 > tend ? $6 : tend)
+      $9 == qstrand && $10 == tstrand &&
+      ($2 - qend <= d || qstart - $3 <= d)) {
+
+    # Extend query block
+    qstart = (qstart < $2) ? qstart : $2
+    qend   = (qend   > $3) ? qend   : $3
+
+    # Extend target block in both directions
+    tstart = (tstart < $5) ? tstart : $5
+    tend   = (tend   > $6) ? tend   : $6
+
   } else {
-    # Flush previous block
     flush()
-    # Start new block
-    qname = $1
-    qstart = $2
-    qend = $3
-    tname = $4
-    tstart = $5
-    tend = $6
+    # New block
+    qname = $1; qstart = $2; qend = $3
+    tname = $4; tstart = $5; tend = $6
+    qstrand = $9; tstrand = $10
   }
 }
 END { flush() }
